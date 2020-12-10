0e6c05a62879bce1da6e0d6ef91f1a3f02a01f5f
jelle <j.g.j.faber@student.utwente.nl>
2020-12-10 12:28:27 +0100

    added graph it looks dope

--- app/src/main/java/no/nordicsemi/android/nrfthingy/visualisationgraph.java
+++
@@ -1,4 +0,0 @@
-package no.nordicsemi.android.nrfthingy;
-
-public class visualisationgraph {
-}
--- app/src/main/java/no/nordicsemi/android/nrfthingy/visualisationpic.java
+++ app/src/main/java/no/nordicsemi/android/nrfthingy/visualisationpic.java
@@ -6,22 +6,45 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import java.util.ArrayList;
+
 public class visualisationpic extends AppCompatActivity {
+    BarChart barChart;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.visualisationpicture);
-        TextView soundwoord;
-        TextView timewoord;
-        TextView thingywoord;
-        TextView aplitudewoord;
-        ImageView soundpicture;
-        soundpicture = (ImageView) findViewById(R.id.imageView);
-        soundwoord = (TextView) findViewById(R.id.textView);
-        thingywoord = findViewById(R.id.thingy);
-        aplitudewoord = findViewById(R.id.amplitude);
-        soundpicture = (ImageView) findViewById(R.id.imageView);
-        soundwoord = (TextView) findViewById(R.id.textView);
-        thingywoord = findViewById(R.id.thingy);
-        aplitudewoord = findViewById(R.id.amplitude);
-        timewoord = findViewById(R.id.time);
+        barChart = (BarChart) findViewById(R.id.bargraph);
+        ArrayList<BarEntry>barEntries =new ArrayList<>();
+        barEntries.add(new BarEntry(44f,0));
+        barEntries.add(new BarEntry(88f,1));
+        BarDataSet barDataSet = new BarDataSet(barEntries,"thingies");
+        ArrayList<String> thethingies = new ArrayList<>();
+        thethingies.add("thingy1");
+        thethingies.add("thingy2");
+        thethingies.add("thingy3");
+        thethingies.add("thingy4");
+        thethingies.add("thingy5");
+        thethingies.add("thingy6");
+        thethingies.add("thingy7");
+        thethingies.add("thingy8");
+        thethingies.add("thingy9");
+        thethingies.add("thingy10");
+        thethingies.add("thingy11");
+        thethingies.add("thingy12");
+        thethingies.add("thingy13");
+        thethingies.add("thingy14");
+        thethingies.add("thingy15");
+        thethingies.add("thingy16");
+        thethingies.add("thingy17");
+        thethingies.add("thingy18");
+        thethingies.add("thingy19");
+        thethingies.add("thingy20");
+        BarData theData = new BarData(thethingies,barDataSet);
+        barChart.setData(theData);
+
     }}
